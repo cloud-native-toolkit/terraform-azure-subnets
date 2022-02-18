5fc2199b1d40ebb407ffff9279fde3ca5457733f
Sean Sundberg <seansund@us.ibm.com>
2022-02-17 21:10:24 -0800

    Fixes test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-vpc.tf
+++ test/stages/stage1-vpc.tf
@@ -5,6 +5,6 @@ module "vpc" {
   region              = var.region
   name_prefix         = var.name_prefix
   address_prefix_count = 1
-  address_prefixes    = ["10.0.0.0/16"]
+  address_prefixes    = ["10.0.0.0/8"]
   enabled             = module.resource_group.enabled
 }
